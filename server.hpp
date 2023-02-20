9fbbae8dae25ae6431b93db9ce325e889d446979
yz723 <yz723@duke.edu>
2023-02-20 14:51:03 -0500

    Delete server.hpp

--- server.hpp
+++
@@ -1,4 +0,0 @@
-int createSocket();
-void initializeListenSocket(int server_fd, struct sockaddr_in address, int addrlen);
-int createCommunicationSocket(int server_fd,  struct sockaddr_in address, int addrlen);
-void greetToClient(int communicate_fd);
