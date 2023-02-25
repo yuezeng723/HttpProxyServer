319af6d7ecee38347c21f7c4b024219c7f010c36
yz723 <yz723@duke.edu>
2023-02-25 18:52:41 -0500

    add LRUCache

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
 CXX = g++
-CXXFLAGS = -g
+CXXFLAGS = -g -std=c++11
 
 all: main
 
-main: main.o proxy.o client.o filelogger.o
-	$(CXX) $(CXXFLAGS) -o main main.o proxy.o client.o filelogger.o -lpthread
+main: main.o proxy.o client.o filelogger.o cache.o
+	$(CXX) $(CXXFLAGS) -o main main.o proxy.o client.o filelogger.o cache.o -lpthread
 
 main.o: main.cpp main.hpp proxy.hpp
 	$(CXX) $(CXXFLAGS) -c main.cpp
 
-proxy.o: proxy.cpp proxy.hpp constant.hpp client.hpp filelogger.hpp request.hpp
+proxy.o: proxy.cpp proxy.hpp constant.hpp client.hpp filelogger.hpp request.hpp cache.hpp
 	$(CXX) $(CXXFLAGS) -c proxy.cpp -lpthread
 
 client.o: client.cpp client.hpp constant.hpp
@@ -18,6 +18,8 @@ client.o: client.cpp client.hpp constant.hpp
 filelogger.o: filelogger.cpp filelogger.hpp client.hpp
 	$(CXX) $(CXXFLAGS) -c filelogger.cpp -lpthread
 
+cache.o: cache.cpp cache.hpp
+	$(CXX) $(CXXFLAGS) -c cache.cpp -lpthread
 .PHONY:
 	clean
 clean:
--- proxy.hpp
+++ proxy.hpp
@@ -24,6 +24,7 @@
 // #include "client.hpp"
 #include "filelogger.hpp"
 #include "request.hpp"
+#include "cache.hpp"
 
 using namespace std;
 namespace http = boost::beast::http;
@@ -39,10 +40,11 @@ private:
    int clientId;
    unordered_map<string, int> ipToIdMap;//key: client's ip ; value: client's id
    Filelogger logger;
+   LRUCache<string, pair<string, http::response<http::dynamic_body>>> cache;
 
    mutex logMutexLock;//a read-write lock to protect proxy.log file.
 public:
-    Proxy():logger("./proxy.log"){
+    Proxy():logger("./proxy.log"), cache(10){
         memset(&host_info, 0, sizeof(host_info));
         host_info.ai_family = AF_UNSPEC;
         host_info.ai_socktype = SOCK_STREAM;
@@ -55,7 +57,7 @@ public:
         clientId = 0;
         initializeServerSocket();
     }
-    Proxy(string port): portnumber(port.c_str()), logger("./proxy.log"){
+    Proxy(string port, size_t cacheSize): portnumber(port.c_str()), logger("./proxy.log"), cache(cacheSize){
         memset(&host_info, 0, sizeof(host_info));
         host_info.ai_family = AF_UNSPEC;
         host_info.ai_socktype = SOCK_STREAM;
